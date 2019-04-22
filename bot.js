a165503393d77514ef4d75fb7a47bd8fa3661963
jsmithtech2019 <jsmithtech2019@gmail.com>
2019-04-22 03:54:03 -0500

    Rework of code

--- bot.js
+++ bot.js
@@ -13,22 +13,19 @@ var apiKey = process.env.GIPHY_API_KEY;
 // API key for Wolfram Alpha
 var wolfApiKey = process.env.WOLFRAM_APP_ID;
 
+// Collection of commands that Jenkins can be triggered on
+// Longest command length is 7 characters
+var helpCommand = '/help';
+var giphyCommand = '/giphy';
+var lmgtfyCommand = '/lmgtfy';
+var xkcdCommand = '/xkcd';
+var wolframCommand = '/wolf';
+var jenkinsCommand = '/jenkins';
+var clearCommand = '/clear';
+var allCommand = '/all';
 
-
-//************************** Methods *****************************************
-// Main function that parses input from Groupme users
-function respond() {
-
-  // Accepted commands by the bot
-  var request = JSON.parse(this.req.chunks[0]),
-    helpCommand = '/help',
-    giphyCommand = '/giphy',
-    lmgtfyCommand = '/lmgtfy',
-    xkcdCommand = '/xkcd',
-    wolframCommand = '/wolf',
-    // Posts a really bad image that can't be removed, ignore this value with Giphy
-    bannedHalal = 'halal';
-  var butlerJokes = ['You rang sir?',
+// Array of different butler statements
+var butlerJokes = ['You rang sir?',
                      'Those who choose to be servants know the most about being free.',
                      'Am I really the only servant here?',
                      'You can\'t unfry an egg, sir.',
@@ -37,89 +34,97 @@ function respond() {
                      'Good evening, Colonel. Can I give you a lift?',
                      'You are not authorized to access this area.'];
 
-  // Do nothing if there is no text
-  /*if(!(request.text)){
-    // Do nothing if there is no text
-    this.res.writeHead(200);
-    this.res.end();*/
+// Help message for bot usage
+var helpMessage = 'Usage instructions for your Butler:\n\
+  "/help"     Posts this help message.\n\
+  "/giphy"   Posts a relevant Gif.\n\
+  "/xkcd"    Finds a relevant XKCD comic.\n\
+  "/clear"   Clears the chat history.\n\
+  "/lmgtfy" Posts dumb question response.\n\
+  "/wolf"    Finds Answer on Wolfram Alpha.\n\
+  "/reddit" *BETA* Posts related\n                      Reddit comment.';
 
-  // Print the usage instructions for the Jenkins Butler
-  if(request.text && request.text.substring(0, helpCommand.length) === helpCommand){
+// Posts a really bad image that can't be removed, ignore this value with Giphy
+var bannedHalal = 'halal';
+
+// Confused Nick Young Face Image
+var confusedNickYoung = 'https://i.kym-cdn.com/entries/icons/mobile/000/018/489/nick-young-confused-face-300x256-nqlyaa.jpg';
+
+//************************** Methods *****************************************
+// Main function that parses input from Groupme users
+function parse() {
+  var request = JSON.parse(this.req.chunks[0])
+  var command = ''
+  // Verify there is text in the message (not an image or blank)
+  if (request.text){
+    // Grab only first word of the request
+    command = request.text.toLowerCase().replace(/ .*/,'');
+
+    // Parse to find the proper function to reply with
+    switch(command) {
+      case helpCommand:
+        this.res.writeHead(200);
+        postMessage(helpMessage);
+        this.res.end();
+        break;
+      case lmgtfyCommand:
+        this.res.writeHead(200);
+        letMeGoogleThatForYou(request.text.substring(lmgtfyCommand.length + 1))
+        this.res.end();
+        break;
+      case giphyCommand:
+        // Ensure an image was requested and not just the command
+        if(request.text.length > giphyCommand.length + 1){
+          this.res.writeHead(200);
+          searchGiphy(request.text.substring(giphyCommand.length + 1));
+          this.res.end();
+        }
+        break;
+      case xkcdCommand:
+        // Ensure a comic was requested and not just a blank command
+        if(request.text.length > xkcdCommand.length + 1){
+          this.res.writeHead(200);
+          searchXKCD(request.text.substring(xkcdCommand.length + 1));
+          this.res.end();
+        }
+        break;
+      case wolframCommand:
+        // Ensure a request was made and not just a blank command
+        if(request.text.length > wolframCommand.length + 1){
+          this.res.writeHead(200);
+          askWolfram(request.text.substring(wolframCommand.length + 1));
+          this.res.end();
+        }
+        break;
+      case clearCommand:
+        this.res.writeHead(200);
+        // Buffer is returns 34 on Iphone XS Max
+        postMessage(" \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nChat cleared.");
+        this.res.end();
+        break;
+      case allCommand:
+        // Not yet implemented, need a database of active user ID's up first
+        break;
+      case jenkinsCommand:
+        this.res.writeHead(200);
+        postMessage(butlerJokes[Math.floor(Math.random() * butlerJokes.length)]);
+        this.res.end();
+        break;
+    }
+  }
+
+  // Post Confused Nick Young if anyone says 'wut' in the chat
+  else if (request.text.toLowerCase().includes('wut')) {
     this.res.writeHead(200);
-    postMessage('Usage instructions for your Butler:\n\
-      "/help"     Posts this help message.\n\
-      "/giphy"   Posts a relevant Gif.\n\
-      "/xkcd"     Finds a relevant XKCD comic.\n\
-      "/lmgtfy" Posts the answer to\n                      stupid questions.\n\
-      "/wolf"    Finds Answer on\n                         Wolfram Alpha.\n\
-      "/reddit" *BETA* Posts related\n                      Reddit comment.');
+    postMessage(confusedNickYoung);
     this.res.end();
+  }
 
-  // Provides a Let Me Google That For You link to the requested query
-  } else if(request.text && request.text.length > lmgtfyCommand.length &&
-      request.text.substring(0, lmgtfyCommand.length) === lmgtfyCommand){
-    this.res.writeHead(200);
-    letMeGoogleThatForYou(request.text.substring(lmgtfyCommand.length + 1));
-    this.res.end();
-
-  // Searches and responds with proper Giphy gif for provided query
-  } else if(request.text && request.text.length > giphyCommand.length &&
-     request.text.toLowerCase().substring(giphyCommand.length + 1).includes('halal') == 0 &&
-     request.text.substring(0, giphyCommand.length) === giphyCommand){
-    this.res.writeHead(200);
-    searchGiphy(request.text.substring(giphyCommand.length + 1));
-    this.res.end();
-
-  // Searches for a relevant XKCD comic to provided query
-  } else if(request.text && request.text.length > xkcdCommand.length &&
-      request.text.substring(0, xkcdCommand.length) === xkcdCommand){
-    this.res.writeHead(200);
-    searchXKCD(request.text.substring(xkcdCommand.length + 1));
-    this.res.end();
-
-  // Searches Wolfram Alpha and returns the answer to a question
-  } else if(request.text &&
-      request.text.length > wolframCommand.length &&
-      request.text.toLowerCase().substring(1, wolframCommand.length).includes('wolf') === true &&
-      request.text.substring(0, wolframCommand.length) === wolframCommand){
-    this.res.writeHead(200);
-    askWolfram(request.text.substring(wolframCommand.length + 1));
-    this.res.end()
-
-  // Does Jenkins stuff
-  } else if(request.text &&
-            request.text.toLowerCase().substring(1, '/jenkins'.length).includes('jenkins') === true &&
-            request.text.toLowerCase().substring(0, '/jenkins'.length) === '/jenkins'){
-    this.res.writeHead(200);
-    postMessage(butlerJokes[Math.floor(Math.random() * butlerJokes.length)]);
-    this.res.end();
-
-  // Do nothing
-  } else {
+  else {
+    // Do nothing
     this.res.writeHead(200);
     this.res.end();
   }
-}
-
-// Query Wolfram API for answer to a question
-function askWolfram(query) {
-  options = {
-    hostname: 'api.wolframalpha.com',
-    path: '/v1/result?appid=' + wolfApiKey + '&i=' + encodeQuery(query)
-  };
-
-  var callback = function(response) {
-    var str = '';
-
-    response.on('data', function(chunk){
-      str += chunk;
-    });
-
-    response.on('end', function() {
-      postMessage(str);
-    });
-  }
-  HTTP.request(options, callback).end();
 }
 
 // Query Giphy API for a gif
@@ -164,6 +169,30 @@ function searchXKCD(query) {
     .catch(err => postMessage('Could not find a relevant xkcd'));
 }
 
+// Query Wolfram API for answer to a question
+function askWolfram(query) {
+  // Replace '+' with 'plus' to avoid misreadings
+  query = query.split('+').join('plus');
+
+  options = {
+    hostname: 'api.wolframalpha.com',
+    path: '/v1/result?appid=' + wolfApiKey + '&i=' + encodeQuery(query)
+  };
+
+  var callback = function(response) {
+    var str = '';
+
+    response.on('data', function(chunk){
+      str += chunk;
+    });
+
+    response.on('end', function() {
+      postMessage(str);
+    });
+  }
+  HTTP.request(options, callback).end();
+}
+
 // Create a sassy response (URL) to a dumb question
 function letMeGoogleThatForYou(textToGoogle) {
   var lmgtfyURL = 'http://lmgtfy.com/?q=' + encodeQuery(textToGoogle);
@@ -182,9 +211,7 @@ function encodeQuery(query) {
 
 // Post message to the Groupme chat
 function postMessage(message) {
-  var botResponse, options, body, botReq;
-
-  botResponse = message;
+  var options, body, botReq, botResponse = message;
 
   options = {
     hostname: 'api.groupme.com',
@@ -216,4 +243,4 @@ function postMessage(message) {
   botReq.end(JSON.stringify(body));
 }
 
-exports.respond = respond;
+exports.parse = parse;
